dca8a6ac0ee5fb07d17ede458a13fde8367cc534
neerajsunil2009-sketch <neerajsunil2009@gmail.com>
2026-05-27 16:09:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1135,7 +1135,7 @@ const manualPhotos = [
     download: 'images/564078654_17907928512249826_6116639868985099266_n.jpg',
     aspect: 'pc',
     author: 'Akshay',
-    tags: ['car', 'gtr', 'trending', 'pc wallpaper', '4k wallpaper', 'all'],
+    tags: ['car&bike', 'gtr', 'trending', 'pc wallpaper', '4k wallpaper', 'all'],
     isTrending: true
   },
   {
@@ -1144,7 +1144,7 @@ const manualPhotos = [
     download: 'images/564084566_17907928515249826_5741429816216012430_n.jpg',
     aspect: 'pc',
     author: 'Akshay',
-    tags: ['car', 'gtr', 'trending', 'pc wallpaper', '4k wallpaper', 'all'],
+    tags: ['car&bike', 'gtr', 'trending', 'pc wallpaper', '4k wallpaper', 'all'],
     isTrending: true
   },
   {
@@ -1153,7 +1153,7 @@ const manualPhotos = [
     download: 'images/564999087_17907928497249826_5107958419468349721_n.jpg',
     aspect: 'pc',
     author: 'Akshay',
-    tags: ['car', 'porsche gt3', 'trending', 'pc wallpaper', '4k wallpaper', 'all'],
+    tags: ['car&bike', 'porsche gt3', 'trending', 'pc wallpaper', '4k wallpaper', 'all'],
     isTrending: true
   },
   {
@@ -1162,7 +1162,7 @@ const manualPhotos = [
     download: 'images/564084582_17907928524249826_4957265001454890996_n.jpg',
     aspect: 'pc',
     author: 'Akshay',
-    tags: ['car', 'mustang', 'trending', 'pc wallpaper', '4k wallpaper', 'all'],
+    tags: ['car&bike', 'mustang', 'trending', 'pc wallpaper', '4k wallpaper', 'all'],
     isTrending: true
   },
   {
@@ -1171,7 +1171,7 @@ const manualPhotos = [
     download: 'images/563354395_17907928533249826_7779471007221877190_n.jpg',
     aspect: 'pc',
     author: 'Akshay',
-    tags: ['car', 'amg', 'benze', 'trending', 'pc wallpaper', '4k wallpaper', 'all'],
+    tags: ['car&bike', 'amg', 'benze', 'trending', 'pc wallpaper', '4k wallpaper', 'all'],
     isTrending: true
   }
     
